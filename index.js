4eb3abe6af4fd3d2039dbb927b47bfbf3591e05c
2217lizz <lisbon2217@gmail.com>
2022-06-06 17:11:40 +0200

    created functions for the decreament and increament of input values

---
+++ index.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded",()=>{
+console.log("content loaded")
+
+const plusOperator=document.querySelector("#addition")
+const minusOperator=document.querySelector("#subtraction")
+
+function adder(){
+const result=document.querySelector("#result")
+console.log(result.value)
+
+}
+
+
+
+})
